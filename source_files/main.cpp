fd618e11ba3d66c835189682a48b12b9adc2ae64
AGENT0306 <reitrebicka74@gmail.com>
2025-01-05 21:56:32 -0500

    removed unneeded code

--- source_files/main.cpp
+++ source_files/main.cpp
@@ -1,10 +1,12 @@
 #include "../header_files/NN.h"
 
-const std::string mnistPath = "C:/Coding_Projects/C++/CPP_TorchNN/out/mnist";
+const std::string mnistPath = "../mnist";
 
-const int batches = 1;
+const int batches = 1; // how many batches to train on
 
 int main(){
+  //checks if CUDA GPU is available
+  //if not will set device to CPU
   torch::Device device = torch::kCPU;
   if (torch::cuda::is_available()) {
     std::cout << "CUDA is available! Training on GPU!" << std::endl;
@@ -13,42 +15,39 @@ int main(){
     std::cout << "ERROR!!! Training on CPU." << std::endl;
   }
 
-  NNet* net = new NNet(28, 10);
+  NNet* net = new NNet(28, 10); // defines neural network
 
-  net->to(device);
+  net->to(device); //sets active device to CPU or GPU
 
-  auto dataset = torch::data::datasets::MNIST(mnistPath)
+  auto dataset = torch::data::datasets::MNIST(mnistPath) // loads MNIST dataset
     .map(torch::data::transforms::Stack<>());
 
-  auto data_loader = torch::data::make_data_loader(
+  auto data_loader = torch::data::make_data_loader( //creates data batches for training
     std::move(dataset),
     torch::data::DataLoaderOptions().batch_size(64).workers(2));
-  int i = 0;
   
-  torch::optim::Adam net_optimizer(net->parameters(), torch::optim::AdamOptions(2e-4));
-  //std::cout << "TEST 1!!" << '\n';
+  torch::optim::Adam net_optimizer(net->parameters(), torch::optim::AdamOptions(2e-4)); // optimizer for neural network
+                                                                                                          // handles the models' learning/gradients
+
+  int i = 0;
+
   for(torch::data::Example<>& batch : *data_loader){
     if(i == batches){
       break;
     }
-    //net->zero_grad();
-    std::cout << "Test 2" << '\n';
-    torch::Tensor outputs = net->forward(batch.data.to(device));
-    //std::cout << outputs << std::endl;
-    outputs = outputs.squeeze();
-    outputs = torch::mean(outputs, 1);
-    std::cout << "Test 3" << '\n';
-    std::cout << "outputs shape: " << outputs.sizes() << std::endl;
-    std::cout << "batch.target shape: " << batch.target.sizes() << std::endl;
-    //torch::Tensor loss = torch::binary_cross_entropy(outputs, batch.target).to(device);
-    std::cout << "Test 4" << '\n';
-    //loss.backward();
-    //net_optimizer.step();
-    
-    
-    //std::cout << batch.data;
+    net->zero_grad();
+
+    torch::Tensor outputs = net->forward(batch.data.to(device)); // this runs neural network
+
+    outputs = outputs.squeeze(); // removes extra dimensions
+    outputs = torch::mean(outputs, 1); // will change to find highest value later, currently just finds mean
+    std::cout << "test 4" << std::endl;
+    torch::Tensor loss = torch::binary_cross_entropy(outputs, batch.target).to(device); // calculates loss of neural network
+
+    loss.backward(); //runs backpropagation
+    net_optimizer.step(); //updates weights
+
     std::cout << outputs << '\n';
-    //std::cout << loss << '\n';
     i++;
   }
 
